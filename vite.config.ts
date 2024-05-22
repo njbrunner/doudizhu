2ab49855521050d05129e88ebaf8ce3430f02ff5
Nicholas Brunner <nicholas.brunner@jhuapl.edu>
2024-05-22 12:59:28 -0600

    Setting base for deployment to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/<REPO>/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
